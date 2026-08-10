2d65177e6b2975533c0001d0b8c5131b4bbe55a0
Kohindgs <135346286+Kohindgs@users.noreply.github.com>
2026-08-11 00:04:29 +0530

    Fix Hostinger SIGABRT - limit to 1 CPU worker, add dgeniussolutions.com image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.dgeniussolutions.com',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dgeniussolutions.com',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
